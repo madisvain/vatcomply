d9ca3749924da0b6095df9f6725d379b159d778d
Madis Väin <madisvain@gmail.com>
2023-08-29 22:25:04 +0300

    fix json loading

--- app.py
+++ app.py
@@ -283,13 +283,12 @@ async def rates(request):
             .order_by(Rates.c.date.desc())
             .limit(1)
         )
-        record_rates = json.loads(record.rates)
 
         # Base re-calculation
         rates = {"EUR": 1}
-        rates.update(record_rates)
+        rates.update(record.rates)
         if query.base and query.base != "EUR":
-            base_rate = Decimal(record_rates[query.base])
+            base_rate = Decimal(record.rates[query.base])
             rates = {
                 currency: Decimal(rate) / base_rate for currency, rate in rates.items()
             }
