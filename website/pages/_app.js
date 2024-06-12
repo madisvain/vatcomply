f1320d6a1ca8311ca6fc178a93887fda8130528d
Madis Väin <madisvain@gmail.com>
2024-06-12 21:42:09 +0300

    replace ga with plausible

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,16 +1,8 @@
 import "../styles/base.scss";
 
 import App from "next/app";
-import Router from "next/router";
-import { initGA, logPageView } from "../components/analytics";
 
 export default class VATComplyApp extends App {
-  componentDidMount() {
-    initGA();
-    logPageView();
-    Router.events.on("routeChangeComplete", logPageView);
-  }
-
   render() {
     const { Component, pageProps } = this.props;
     return (
