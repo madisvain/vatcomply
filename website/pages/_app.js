4e959997f7d449800fe4e60a24b01d1bbd62ef7a
Madis Väin <madisvain@gmail.com>
2024-06-12 21:39:32 +0300

    add plausible

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -13,6 +13,11 @@ export default class VATComplyApp extends App {
 
   render() {
     const { Component, pageProps } = this.props;
-    return <Component {...pageProps} />;
+    return (
+      <>
+        <Component {...pageProps} />
+        <script defer data-domain="vatcomply.com" src="https://plausible.io/js/script.js"></script>
+      </>
+    );
   }
 }
